052a4f158c48cf4492b7ffc99c6a51bff04dc047
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-07-01 14:50:35 +1000

    setup backend

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+// load the environment variables from the .env file
+dotenv.config({
+  path: '.env'
+});
+
+const app = express();
+
+// make server listen on some port
+((port = process.env.APP_PORT || 5000) => {
+  app.listen(port, () => console.log(`> Listening on port ${port}`));
+})();
