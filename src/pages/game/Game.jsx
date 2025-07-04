1b870b65c47ddfdc8f6f8f8a23bae4e715818254
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-07-04 12:42:45 +1000

    added dynamically sized grid for solution to be typed in

--- src/pages/game/Game.jsx
+++ src/pages/game/Game.jsx
@@ -3,22 +3,41 @@ import { differenceInCalendarDays } from 'date-fns';
 import wordObject from '../../assets/words.json';
 import './Game.css';
 
-function Game() {
-  const [word, setWord] = useState();
-  const wordArray = Object.keys(wordObject);
-  const baseDate = new Date('2025-07-04');
+const wordArray = Object.keys(wordObject);
+const baseDate = new Date('2025-07-04');
 
-  // Get new word for the day on mounting
+function Game() {
+  // USE LOCAL STORAGE TO SAVE USER STATE!!!!
+  const [solution, setSolution] = useState();
+  const [grid, setGrid] = useState([]);
+
+  // Get the solution for the day and generate grid on mounting
   useEffect(() => {
     const wordIndex = differenceInCalendarDays(new Date(), baseDate) % wordArray.length;
-    console.log(differenceInCalendarDays(new Date(), baseDate));
-    // USE LOCAL STORAGE TO SAVE USER STATE!!!!
-    setWord(wordArray[wordIndex]);
+    const currSolution = wordArray[wordIndex];
+    setSolution(currSolution);
+
+    // For a n-letter word, n + 1 guesses (rows)
+    const emptyGrid = Array.from({ length: currSolution.length + 1 }, () =>
+      new Array(currSolution.length).fill('')
+    );
+    setGrid(emptyGrid);
   }, []);
 
   return (
     <>
-        {word}
+      <div className='grid'>
+        {grid.map((row, rowIndex) => (
+          <div className='row' key={rowIndex}>
+            {row.map((col, colIndex) => (
+              <div className='col' key={colIndex}>
+                {col}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <p>Solution: {solution}</p>
     </>
   );
 }
