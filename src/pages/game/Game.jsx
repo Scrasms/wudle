22959934eea619ae4ffdaf0b8246a266c93a4c7c
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-07-05 14:30:10 +1000

    added word insertion and deletion logic to grid

--- src/pages/game/Game.jsx
+++ src/pages/game/Game.jsx
@@ -4,12 +4,15 @@ import wordObject from '../../assets/words.json';
 import './Game.css';
 
 const wordArray = Object.keys(wordObject);
+// Update this later to the date of deployment
 const baseDate = new Date('2025-07-04');
 
 function Game() {
   // USE LOCAL STORAGE TO SAVE USER STATE!!!!
   const [solution, setSolution] = useState();
   const [grid, setGrid] = useState([]);
+  const [currRow, setCurrRow] = useState(0);
+  const [currCol, setCurrCol] = useState(0);
 
   // Get the solution for the day and generate grid on mounting
   useEffect(() => {
@@ -19,10 +22,62 @@ function Game() {
 
     // For a n-letter word, n + 1 guesses (rows)
     const emptyGrid = Array.from({ length: currSolution.length + 1 }, () =>
-      new Array(currSolution.length).fill('')
+      new Array(currSolution.length).fill(<div></div>)
     );
     setGrid(emptyGrid);
   }, []);
+
+  const handleKey = (event) => {
+    if (event.key === 'Backspace') {
+      clearGrid(event);
+    } else {
+      fillGrid(event);
+    }
+  }
+
+  // Deletes last input from grid
+  const clearGrid = (event) => {
+    let tempRow = currRow;
+    let tempCol = currCol;
+    if (event.key === 'Backspace') {
+      if (currCol === 0) {
+        if (currRow === 0) return;
+        tempRow = currRow - 1;
+        tempCol = solution.length - 1;
+      } else {
+        tempCol = currCol - 1;
+      }
+      insertGrid(tempRow, tempCol, null);
+      setCurrRow(tempRow);
+      setCurrCol(tempCol);
+    }
+  }
+
+  // Adds new input to grid
+  const fillGrid = (event) => {
+    // Only letters are allowed
+    if (event.key.length !== 1 || !event.key.match(/[a-z]/i)) return;
+
+    // Do nothing if on last grid square
+    if (currRow === solution.length + 1 && currCol === 0) return;
+
+    // Fill grid with user input
+    insertGrid(currRow, currCol, <div className='grid-text'>{event.key}</div>);
+
+    if (currCol === solution.length - 1) {
+      setCurrRow(currRow + 1);
+      setCurrCol(0);
+    } else {
+      setCurrCol(currCol + 1);
+    }
+  }
+
+  // Inserts new element to the grid at specified row and col
+  const insertGrid = (row, col, element) => {
+    const newGrid = grid.map(row => [...row]);
+    newGrid[row][col] = <div className='grid-text'>{element}</div>;
+    setGrid(newGrid);
+  }
 
   return (
     <>
@@ -38,6 +93,7 @@ function Game() {
         ))}
       </div>
       <p>Solution: {solution}</p>
+      <input onKeyDown={handleKey} />
     </>
   );
 }
